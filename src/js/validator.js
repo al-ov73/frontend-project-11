3cebb098f1adf769aa07b59cfa424b7120235c6f
al-ov73 <klever1987@yandex.ru>
2024-04-09 20:21:28 +0400

    add network error

--- src/js/validator.js
+++ src/js/validator.js
@@ -1,45 +1,45 @@
 import * as yup from 'yup';
-import axios from 'axios';
 
-const isRss = async (link) => {
-  try {
-    return fetch(`https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(link)}`)
-    .then(response => {
-      if (response.ok) return response.json()
-      throw new Error('Network response was not ok.')
-    })
-    .then((jsonResult) => {
-      const parser = new DOMParser();
-      const content = jsonResult.contents
-      const doc = parser.parseFromString(content, "text/html");
-      if (doc.querySelector('channel')) {
-        return true;
-      }
-      return false;
-    })
-  } catch (err) {
-      state.form = {
-        isValid: false,
-        validationResult: 'networkError',
-      }
+const getResponse = async (link, state) => {
+  return fetch(`https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(link)}`)
+  .then(response => {
+    if (response.ok) return response.json()
+    throw new Error('Network response was not ok.')
+  })
+  .catch((error) => {
+    state.form = {
+      isValid: false,
+      validationResult: 'networkError',
+    }
+  });
+};
+
+const isRss = (response) => {
+  const parser = new DOMParser();
+  const content = response.contents
+  const doc = parser.parseFromString(content, "text/html");
+  if (doc.querySelector('channel')) {
+    return true;
   }
+  return false;
 };
 
 export default async (inputUrlObj, state) => {
-
   const schema = yup.object({
     url: yup.string().url().nullable(),
   });
   schema.validate(inputUrlObj)
-    .then( async () => {
+    .then(async () => {
       if (state.RssLinks.includes(inputUrlObj.url)) {
         state.form = {
           isValid: false,
           validationResult: 'urlExist',
         }
       } else {
-        isRss(inputUrlObj.url)
-        .then((RssValidatResult) => {
+        getResponse(inputUrlObj.url, state)
+        .then((content) => {
+          console.log('content', content)
+          const RssValidatResult = isRss(content);
           if (RssValidatResult) {
             state.form = {
               isValid: true,
@@ -53,7 +53,7 @@ export default async (inputUrlObj, state) => {
             }
           }
         })
-
+        .catch(() => {});
       }
     })
     .catch(() => {
