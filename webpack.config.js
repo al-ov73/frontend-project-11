0474f1cbd91d6a50870c370710e1156235fb3766
al-ov73 <klever1987@yandex.ru>
2024-04-10 12:31:49 +0400

    corrent linter in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,62 +3,61 @@ const autoprefixer = require('autoprefixer');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 
-const isProduction = process.env.NODE_ENV == 'production';
-
+const isProduction = process.env.NODE_ENV === 'production';
 
 const config = {
-    entry: './src/js/index.js',
-    output: {
-        filename: 'index.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    devServer: {
-        static: path.resolve(__dirname, 'dist'),
-        port: 8080,
-        hot: true,
-        allowedHosts: "all",
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './index.html',
-        }),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif|scss)$/i,
-                use: [
-                    {
-                      loader: 'style-loader'
-                    },
-                    {
-                      loader: 'css-loader'
-                    },
-                    {
-                      loader: 'postcss-loader',
-                      options: {
-                        postcssOptions: {
-                          plugins: [
-                            autoprefixer
-                          ]
-                        }
-                      }
-                    },
-                    {
-                      loader: 'sass-loader'
-                    }
-                  ]
+  entry: './src/js/index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    port: 8080,
+    hot: true,
+    allowedHosts: 'all',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif|scss)$/i,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  autoprefixer,
+                ],
+              },
             },
+          },
+          {
+            loader: 'sass-loader',
+          },
         ],
-    },
+      },
+    ],
+  },
 };
 
 module.exports = () => {
-    if (isProduction) {
-        config.mode = 'production';
-        config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
-    } else {
-        config.mode = 'development';
-    }
-    return config;
+  if (isProduction) {
+    config.mode = 'production';
+    config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
+  } else {
+    config.mode = 'development';
+  }
+  return config;
 };
